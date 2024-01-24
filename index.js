68138c62b0dedfd968a523b563003c7afe939972
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-01-24 14:14:54 +0600

    UsersProfile collection added

--- index.js
+++ index.js
@@ -30,8 +30,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const UsersProfileCollection = client.db('HireMaster').collection('UsersProfile');
+
+
+
+
+
+    //  UserProfileCollection
+    app.get('/userProfile',async(req,res)=>{
+      const result = await  UsersProfileCollection.find().toArray();
+    res.send(result);
+    });
+
+    
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
