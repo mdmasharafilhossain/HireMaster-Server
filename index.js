c955f3339c8b231ac2b7fc8584e4d43fbd483519
Mohosin212601~ <mohosin2126@gmail.com>
2024-01-25 22:59:27 +0600

    static data loaded using get method and staticjobpost collection api

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
 
 
     const jobCollection=client.db('HireMaster').collection('jobData')
-
+  const staticCollection=client.db('HireMaster').collection('JobPost')
 
     //  UserProfileCollection
 
@@ -55,6 +55,16 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
   })
+
+
+
+  app.get('/staticjobpost',async(req,res)=>{
+    const cursor =  staticCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+})
+
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
