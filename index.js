936fb85e98f0e9e71f4df1540d943ecd06d6378a
Miran Ibrahim <moeenuddin.miran@gmail.com>
2024-01-26 08:22:07 +0600

    commit for switching

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-// middleware 
- app.use(cors());
+// middleware
+app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzichn4.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,56 +17,56 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const UsersProfileCollection = client.db('HireMaster').collection('UsersProfile');
+    const UsersProfileCollection = client
+      .db("HireMaster")
+      .collection("UsersProfile");
 
-
-    const jobCollection=client.db('HireMaster').collection('jobData')
-  const staticCollection=client.db('HireMaster').collection('JobPost')
+    const jobCollection = client.db("HireMaster").collection("jobData");
+    const staticCollection = client.db("HireMaster").collection("JobPost");
 
     //  UserProfileCollection
 
-    app.post('/userProfile', async(req,res)=>{
+    app.post("/userProfile", async (req, res) => {
       const feedbacks = req.body;
       const result = await UsersProfileCollection.insertOne(feedbacks);
-       res.send(result);
+      res.send(result);
     });
 
-    app.get('/userProfile',async(req,res)=>{
-      const result = await  UsersProfileCollection.find().toArray();
-    res.send(result);
+    app.get("/userProfile", async (req, res) => {
+      const result = await UsersProfileCollection.find().toArray();
+      res.send(result);
     });
 
-    app.post("/jobpost",async(req,res)=>{
-      const job=req.body
-      const result=await jobCollection.insertOne(job)
-      res.send(result)
-    })
+    app.post("/jobpost", async (req, res) => {
+      const job = req.body;
+      const result = await jobCollection.insertOne(job);
+      res.send(result);
+    });
 
-    app.get('/jobpost',async(req,res)=>{
-      const cursor =  jobCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-  })
-
-
-
-  app.get('/staticjobpost',async(req,res)=>{
-    const cursor =  staticCollection.find();
+    app.get("/jobpost", async (req, res) => {
+      const cursor = jobCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-})
+    });
 
+    app.get("/staticjobpost", async (req, res) => {
+      const cursor = staticCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -77,12 +74,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req,res)=>{
-    res.send('HireMaster Server Running Successfully');
+app.get("/", (req, res) => {
+  res.send("HireMaster Server Running Successfully");
 });
 
-app.listen(port,()=>{
-    console.log(`HireMaster Server Running at Port ${port}`)
+app.listen(port, () => {
+  console.log(`HireMaster Server Running at Port ${port}`);
 });
