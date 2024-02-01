e79e9cb1798209d3f5fd0584a77cb18fbf9c78cd
Thouhid98 <thouhidhussain98@gmail.com>
2024-02-01 13:07:24 +0600

    Applied Jobs and Show applied jobs

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware 
- app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzichn4.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,43 +27,56 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const UsersProfileCollection = client.db('HireMaster').collection('UsersProfile');
-
-
-    const jobCollection=client.db('HireMaster').collection('jobData')
-  const staticCollection=client.db('HireMaster').collection('JobPost')
+    const jobCollection = client.db('HireMaster').collection('jobData')
+    const appliedJobCollection = client.db('HireMaster').collection('AppliedJob')
+    const staticCollection = client.db('HireMaster').collection('JobPost')
 
     //  UserProfileCollection
 
-    app.post('/userProfile', async(req,res)=>{
+    app.post('/userProfile', async (req, res) => {
       const feedbacks = req.body;
       const result = await UsersProfileCollection.insertOne(feedbacks);
-       res.send(result);
+      res.send(result);
     });
 
-    app.get('/userProfile',async(req,res)=>{
-      const result = await  UsersProfileCollection.find().toArray();
-    res.send(result);
+    app.get('/userProfile', async (req, res) => {
+      const result = await UsersProfileCollection.find().toArray();
+      res.send(result);
     });
 
-    app.post("/jobpost",async(req,res)=>{
-      const job=req.body
-      const result=await jobCollection.insertOne(job)
+    app.post("/jobpost", async (req, res) => {
+      const job = req.body
+      const result = await jobCollection.insertOne(job)
       res.send(result)
     })
 
-    app.get('/jobpost',async(req,res)=>{
-      const cursor =  jobCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-  })
+    //Applied Jobs 
+    app.post("/users-appliedjobs", async(req, res)=>{
+      const appliedjobs = req.body
+      console.log(appliedjobs);
+      const result = await appliedJobCollection.insertOne(appliedjobs)
+      res.send(result)
+    })
+    // Show Applied Jobs 
+    app.get('/showapplied-jobs', async(req, res)=>{
+      const cursor = appliedJobCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
-
-
-  app.get('/staticjobpost',async(req,res)=>{
-    const cursor =  staticCollection.find();
+    app.get('/jobpost', async (req, res) => {
+      const cursor = jobCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-})
+    })
+
+
+
+    app.get('/staticjobpost', async (req, res) => {
+      const cursor = staticCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
 
     // Connect the client to the server	(optional starting in v4.7)
@@ -79,10 +93,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('HireMaster Server Running Successfully');
+app.get('/', (req, res) => {
+  res.send('HireMaster Server Running Successfully');
 });
 
-app.listen(port,()=>{
-    console.log(`HireMaster Server Running at Port ${port}`)
+app.listen(port, () => {
+  console.log(`HireMaster Server Running at Port ${port}`)
 });
