88c5fbf5c525f03ea200300de57e7598559379e4
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-02-13 16:39:32 +0600

    design added in Job seeker list

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ async function run() {
         const page = query.page;
         console.log(page);
        const pageNumber = parseInt(page);
-        const perPage = 5;
+        const perPage = 4;
         const skip = pageNumber * perPage ;
         const users = userCollection.find().skip(skip).limit(perPage);
       const result = await  users.toArray();
