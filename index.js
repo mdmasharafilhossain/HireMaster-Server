04268d136935d6256f0613bad391507b92858ed9
Miran Ibrahim <moeenuddin.miran@gmail.com>
2024-02-26 02:11:57 +0600

    ssl payment api created

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const SSLCommerzPayment = require("sslcommerz-lts");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 // middleware
@@ -33,6 +34,10 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false; //true for live, false for sandbox
 
 // ----------------middleware----------------------
 const logger = async (req, res, next) => {
@@ -428,6 +433,88 @@ async function run() {
     });
 
     // ---------------------- Admin Dashboard ------------------------
+
+    // --------------------SSL PAYMENT-------------------
+
+    const tran_id = new ObjectId().toString();
+    app.post("/buy-premium", async (req, res) => {
+      const data = {
+        total_amount: req.body.amount,
+        currency: "BDT",
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment-success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment-fail/${tran_id}`,
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: "Customer Name",
+        cus_email: req.body.email,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+      console.log(data);
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
+
+        const finalPayment = {
+          email: req.body.email,
+          name: req.body.name,
+          price: req.body.amount,
+          date: new Date(),
+          transaction_ID: tran_id,
+          paidStatus: false,
+        };
+        const result = UserPaymentCollection.insertOne(finalPayment);
+        console.log("Redirecting to: ", GatewayPageURL);
+      });
+
+      app.post("/payment-success/:tranId", async (req, res) => {
+        console.log(req.params.tranId);
+        const result = await UserPaymentCollection.updateOne(
+          { transaction_ID: req.params.tranId },
+          {
+            $set: {
+              paidStatus: true,
+            },
+          }
+        );
+        if (result.modifiedCount > 0) {
+          res.redirect(
+            `http://localhost:5173/payment-success/${req.params.tranId}`
+          );
+        }
+      });
+
+      app.post("/payment-fail/:tranId", async (req, res) => {
+        const result = await UserPaymentCollection.deleteOne({
+          transaction_ID: req.params.tranId,
+        });
+        if (result.deletedCount > 0) {
+          res.redirect(
+            `http://localhost:5173/payment-fail/${req.params.tranId}`
+          );
+        }
+      });
+    });
 
     // ------------------Stripe Payment--------------------
 
